00df7d0834c4d9a3983b4dd7dfbaefcba55965e6
Ajonwal <85732074+Ajonwal@users.noreply.github.com>
2023-09-16 09:43:48 +0530

    “complete”

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./header.css";
-import { Button } from "../button/Button";
 import { useCartContext } from "../context/cart_context";
 import { useAuth0 } from "@auth0/auth0-react";
 export const Header = () => {
